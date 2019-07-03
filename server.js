3ecc074bba4572ab08091e33e5baf8635a3322f8
parhsmaropoulos <parhsmaro13@gmail.com>
2019-07-03 19:51:27 +0300

    Post with Postman:

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-add.post('/',(req,res)=>{
+
+// Handling all the parsing
+app.use(bodyParser.json())
+
+
+app.post('/',(req,res)=>{
     var email = req.body.email;
     var amount = req.body.amount;
 
